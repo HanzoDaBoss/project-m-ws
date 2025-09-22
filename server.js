89373eb6d6997b2bcb80c90c84647847dd6f087b
HanzoDaBoss <hanif.uddz@gmail.com>
2025-09-22 22:57:43 +0100

    Changed http module to https

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const http = require("http");
+const https = require("https");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const server = http.createServer(app);
+const server = https.createServer(app);
 const {Server} = require("socket.io");
 const io = new Server(server, {
     cors: {
